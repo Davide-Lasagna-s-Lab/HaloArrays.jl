28c82f326f8b1ec800bd631e5bafebcedf9245ff
Thomas Burton <tburton5572@gmail.com>
2026-09-04 10:23:05 +0100

    added fill! method to fix issue with scalar broadcasting

--- ext/CUDAExt.jl
+++ ext/CUDAExt.jl
@@ -29,4 +29,13 @@ function CUDA.cu(a::HaloArray{T, N, NHALO, SIZE}) where {T, N, NHALO, SIZE}
     return HaloArray{NHALO, SIZE}(data_d, a.haloregions, a.comm, a.reqs, a.economic)
 end
 
+Base.fill!(a::HaloArrays.HaloArray{T, N, NHALO, SIZE, <:CuArray}, x) where {T, N, NHALO, SIZE} = (CUDA.fill!(parent(a), x); return a)
+
+# ! see https://github.com/Davide-Lasagna-s-Lab/NSEBase.jl.git ext/MPIExt/derivatives.jl interior_laplacian! method
+# function Base.fill!(a::SubArray{T, N, <:HaloArrays.HaloArray{T, N, NHALO, SIZE, <:CuArray}}, x) where {T, N, NHALO, SIZE}
+#     inner = @view parent(a).data[a.indices...]
+#     fill!(inner, x)
+#     return a
+# end
+
 end
